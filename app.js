0d6c7ff47f1ad262c6bd26510cc055db16e63d56
mpmorgan94 <mpmorgan94@gmail.com>
2021-11-29 02:08:23 -0600

    added security of user access to pages

--- app.js
+++ app.js
@@ -27,12 +27,31 @@ app.get('/login', (req, result) => {
     result.render('pages/landing')
 });
 
-app.get('/home', (req, result) => {
-    result.render('pages/home')
+app.post('/home', async (req, result) => {
+    
+    pool.query(`SELECT * FROM public.User WHERE email='${req.body.email}'
+                AND password='${req.body.password}'`, (err, res) => {
+        if (err) {
+            console.log(err)
+            result.render('pages/errors', { errmsg: err })
+        }
+        else if (res.rowCount == 0) {
+            console.log("no account associated with provided username and password.")
+            result.render('pages/errors', { errmsg: "credential verification failed" })
+        }
+        else {
+            //success, login
+            result.render('pages/home', { email: req.body.email, password: req.body.password })
+        }
+    });
 });
 
 app.get('/signUp', (req, result) => {
     result.render('pages/signUp')
+});
+
+app.get('/errors', (req, result) => {
+    result.render('pages/errors', { errmsg: req.query.errmsg })
 });
 
 app.post('/verifyCreds', (req, result) => {
@@ -47,7 +66,7 @@ app.post('/verifyCreds', (req, result) => {
         }
         else {
             //success, login
-            result.render('pages/home', { currUsername: res.rows[0].email });
+            result.render('pages/home', { email: res.rows[0].email, password: res.rows[0].password });
         }
     });
 });
@@ -74,17 +93,32 @@ app.post('/createUser', (req, result) => {
                 }
                 else {
                     console.log("user created");
-                    result.redirect('/home');
+                    //success, login
+                    result.render('pages/home', { email: `${req.body.email}`, password: `${req.body.password}` });
                 }
             })
         }
     });
 });
 
-app.get('/allBooks', (req, result) => {
-    let books = pool.query('SELECT * FROM book', (err, res) => {
-        result.render('pages/allBooks', { books: res.rows })
-    })
+app.post('/allBooks', (req, result) => {
+    pool.query(`SELECT * FROM public.User WHERE email='${req.body.email}'
+                AND password='${req.body.password}'`, (err, res) => {
+        if (err) {
+            console.log(err)
+            result.render('pages/errors', { errmsg: err })
+        }
+        else if (res.rowCount == 0) {
+            console.log("no account associated with provided username and password.")
+            result.render('pages/errors', { errmsg: "credential verification failed" })
+        }
+        else {
+            //success, allow access
+            let books = pool.query('SELECT * FROM book', (err, res) => {
+                result.render('pages/allBooks', { books: res.rows, email: req.body.email, password: req.body.password });
+            })
+        }
+    });
 });
 
 app.listen(process.env.PORT || 3000, () => {
